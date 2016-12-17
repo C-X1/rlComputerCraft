abd77236eac4fc339ad12fcdfecf9a3480d3f6dc
Christian Holl <cyborgx1@gmail.com>
2016-12-17 15:36:31 +0100

    fix issue with redstone api function

--- rlTurtle/src/rlRedStone.lua
+++ rlTurtle/src/rlRedStone.lua
@@ -1,15 +1,16 @@
+
 function pulseBundleWire(bundle_loc, color)
-    local b_state=redstone.getBundledOutput(bundle_loc)
-    rs.setBoundledOutput(bundle_loc, colors.combine(b_state,color))
-    rs.setBoundledOutput(bundle_loc, colors.subtract(b_state,color))
+    local b_state=redstone.getBundledOutput(bundle_loc)    
+    redstone.setBundledOutput(bundle_loc, colours.combine(b_state,color))
+    redstone.setBundledOutput(bundle_loc, colours.subtract(b_state,color))
 end
 
 function setBundleWire(bundle_loc, color)
     local b_state=redstone.getBundledOutput(bundle_loc)
-    rs.setBoundledOutput(bundle_loc, colors.combine(b_state,color))
+    redstone.setBundledOutput(bundle_loc, colors.combine(b_state,color))
 end
 
 function clearBundleWire(bundle_loc, color)
     local b_state=redstone.getBundledOutput(bundle_loc)
-    rs.setBoundledOutput(bundle_loc, colors.subtract(b_state,color))
+    redstone.setBundledOutput(bundle_loc, colors.subtract(b_state,color))
 end
