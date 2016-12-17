4901d544379d8be2c1032ba6bc40318b87ca81a0
Christian Holl <cyborgx1@gmail.com>
2016-12-17 17:13:02 +0100

    fix issues, change to control redstone engines

--- rlTurtle/src/examples/checkContent.lua
+++ rlTurtle/src/examples/checkContent.lua
@@ -1,25 +1,73 @@
 os.loadAPI("rlOccSense")
-while true do
-  i=rlOccSense.getInventoryPercentFull("top",1,-1,2)
-  pulseState=false
-  on=i<30
- 
-  term.clear()
-  print(i)
- 
+os.loadAPI("rlRedStone")
+
+function checkContentAndBundled(sensor_loc, 
+                                     bundle_loc, 
+                                     contentLimit,
+                                     x,y,z,colour)                            
+  local user_loc_string= x..","..","..y..","..z .. ": "
+  local i=rlOccSense.getInventoryPercentFull(sensor_loc,x,y,z)
   if(i==nil) then
-    print("Error chest not found!")  
-    rs.setOutput("right", false)
-    exit(1)
+    return (user_loc_string .. " not found!")
   end
- 
- 
-  if(on==true)then
-    pulsestate=not pulsestate
-    rs.setOutput("right", pulsestate)    
-    print("ON")  
+  
+  local status=""
+  
+  
+  if(i<contentLimit)then
+    rlRedStone.setBundleWire(bundle_loc, colour)
+    status=" ON"    
   else
-    print("OFF")  
+    rlRedStone.clearBundleWire(bundle_loc, colour)
+    status=" OFF"  
   end
+
+  return (user_loc_string .." ".. status .. " ".. i)
+end
+
+local i=0
+local sense_loc="right"
+local bundle_loc="back"
+
+while true do
+  i=i+1
+  
+  
+
+  term.clear()
+  print("Cycle " .. i .. ": ")
+      
+  rlRedStone.pulseBundleWire(bundle_loc,colors.white)
+  
+  print( "Contrete Supply" ..
+          checkContentAndBundled(
+                                        sense_loc,
+                                        bundle_loc,
+                                        30,
+                                        -1,-1,-4,
+                                        colors.green
+                                     ) 
+        )
+        
+    print("Compact Concrete Gen" ..
+          checkContentAndBundled(
+                                        sense_loc,
+                                        bundle_loc,
+                                        30,
+                                        4,-1,-1,
+                                        colors.gray
+                                     ) 
+         )
+         
+     print( "Reinf Concrete Gen" ..
+          checkContentAndBundled(
+                                        sense_loc,
+                                        bundle_loc,
+                                        30,
+                                        3,0,-6,
+                                        colors.black
+                                     ) 
+        )
+  
   os.sleep(1)
 end
