8e438979090db3dba1115e9886899d4660ea057a
Christian Holl <cyborgx1@gmail.com>
2016-12-17 13:29:02 +0100

    add function to set and clear bundle wire

--- rlTurtle/src/rlRedStone.lua
+++ rlTurtle/src/rlRedStone.lua
@@ -1,5 +1,15 @@
-function pulseBundle(bundle_loc, color)
+function pulseBundleWire(bundle_loc, color)
     local b_state=redstone.getBundledOutput(bundle_loc)
     rs.setBoundledOutput(bundle_loc, colors.combine(b_state,color))
+    rs.setBoundledOutput(bundle_loc, colors.subtract(b_state,color))
+end
+
+function setBundleWire(bundle_loc, color)
+    local b_state=redstone.getBundledOutput(bundle_loc)
+    rs.setBoundledOutput(bundle_loc, colors.combine(b_state,color))
+end
+
+function clearBundleWire(bundle_loc, color)
+    local b_state=redstone.getBundledOutput(bundle_loc)
     rs.setBoundledOutput(bundle_loc, colors.subtract(b_state,color))
 end
