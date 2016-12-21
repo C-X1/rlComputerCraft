d94a92063d3f73ed6ce115618b542a366217ee6f
Christian Holl <cyborgx1@gmail.com>
2016-12-21 05:40:43 +0100

    add some states to main

--- rlTurtle/src/programs/icbm_enderlaunch/el_icbm_mainturtle.lua
+++ rlTurtle/src/programs/icbm_enderlaunch/el_icbm_mainturtle.lua
@@ -1,3 +1,27 @@
+local main_state=
+{
+  waitForChest=0,
+  getFromChest=1,
+  insertDisk=2,
+  waitCanLaunch=3,
+  loadMissile=4,
+  launch=5,
+}
+local s=main_state
+
+state=s.waitForChest
+
 local function main()
+
+    while  true  do
+        if(state==s.waitForChest)then
+        
+        elseif(state==s.getFromChest)then
+        
+        
+        else
+        
+        end
+    end
 
 end
