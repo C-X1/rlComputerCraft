ca6130952d3e06a8d9a4dc5459a029bc958897e8
Christian Holl <cyborgx1@gmail.com>
2016-12-23 01:32:52 +0100

    add chest slot checking

--- rlTurtle/src/programs/icbm_enderlaunch/el_icbm_mainturtle.lua
+++ rlTurtle/src/programs/icbm_enderlaunch/el_icbm_mainturtle.lua
@@ -1,27 +1,63 @@
+os.loadAPI("ocs/apis/sensor")
+
+
 local main_state=
 {
   waitForChest=0,
   getFromChest=1,
-  insertDisk=2,
-  waitCanLaunch=3,
-  loadMissile=4,
+  loadMissile=2,
+  insertDisk=3,
+  waitState=4,
+  
   launch=5,
+  err=6,
 }
-local s=main_state
 
-state=s.waitForChest
+local function getInventorySlot(sensor_loc,place,slot)
+  local sensor = sensor.wrap(sensor_loc)
+  
+  if(sensor==nil)then
+    return nil
+  end
+  
+  details=sensor.getTargetDetails(place)
+  
+  if details == nil then
+    return nil
+  end
+  
+  if(details.Slots == nil) then
+    return nil
+  end
+  
+  return details.Slots[slot]
+end
+
+local function updateSlotState()
+    slot1=getInventorySlot("left","0,-1,0",1)
+    slot2=getInventorySlot("left","0,-1,0",2)
+    
+    if(slot1~=nil)then
+      print(slot1.Name)
+    end
+    
+    if(slot2~=nil)then
+      print(slot2.Name)
+    end
+end
+ 
 
 local function main()
-
-    while  true  do
-        if(state==s.waitForChest)then
-        
-        elseif(state==s.getFromChest)then
-        
-        
-        else
-        
-        end
-    end
-
+  while  true  do
+      if(state==s.waitForChest)then
+      
+      elseif(state==s.getFromChest)then
+      
+      
+      else
+      
+      end
+    os.sleep(0.1)
+  end
 end
+main()
