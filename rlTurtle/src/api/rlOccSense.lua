ad8ef39c0b9b8ae56d218cc91d95d3f91ed4c20f
Christian Holl <cyborgx1@gmail.com>
2017-07-21 02:54:33 +0200

    add range filter for sensor targets

--- rlTurtle/src/api/rlOccSense.lua
+++ rlTurtle/src/api/rlOccSense.lua
@@ -1,5 +1,40 @@
 os.loadAPI("ocs/apis/sensor")
 
+
+function filterInRange(sensor_location, xmin, ymin, zmin, xmax, ymax, zmax)
+  local s = sensor.wrap(sensor_location)
+  local targets={}
+  local tgt=0
+  for target, data in pairs(s.getTargets()) do
+    for dataName, dataValue in pairs(data) do
+        local isTarget=0
+        if(dataName=="Position")then
+            for posDataName, posDataValue in pairs(dataValue) do
+                if(posDataName=='X')then
+                    if(posDataValue>=xmin or posDataValue<=xmax)then
+                      isTarget=isTarget+1
+                    end
+                end
+                if(posDataName=='Y')then
+                    if(posDataValue>=ymin or posDataValue<=ymax)then
+                      isTarget=isTarget+1
+                    end
+                end
+                if(posDataName=='Z')then
+                    if(posDataValue>=zmin or posDataValue<=zmax)then
+                      isTarget=isTarget+1
+                    end
+                end
+            end
+        end
+        if(isTarget==3)then
+            targets[target]=s.getTargetDetails(target)
+        end
+    end
+  end
+  return targets
+end
+
 function filterByPos(sensor_location,x,y,z)
 
   local s = sensor.wrap(sensor_location)
